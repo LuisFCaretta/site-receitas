9fa481dfb5900b16773fde4c488f1ec5520c3bba
LuisFCaretta <lcaretta@yahoo.com>
2023-10-07 14:47:22 -0300

    Separação de testes e pagination criado.

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,16 @@
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
+from django.db.models import Q
+from utils.pagination import make_pagination
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
     })
 
 
@@ -17,9 +20,11 @@ def category(request, category_id):
             is_published=True,
             category__id=category_id).order_by('-id')
     )
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
 
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category '
     })
 
@@ -41,7 +46,21 @@ def search(request):
     search_term = request.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
         'search_term': search_term,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}'
     })
