089d6a847b4dd2ccc67eae1ddbc15fb1039d8889
LuisFCaretta <lcaretta@yahoo.com>
2023-10-06 21:20:30 -0300

    Correção de erros nos testes unitários.

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -14,7 +14,7 @@ class RecipeModelTest(RecipeTestBase):
             author=self.make_author(username='NewUsr'),
             title='Recipe title',
             description='Recipe decription',
-            slug='recipe-title',
+            slug='recipe-title-slug-for-no-defaults',
             preparation_time='10',
             preparation_time_unit='Minutos',
             servings='5',
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
 
@@ -37,4 +38,10 @@ def recipe(request, id):
 
 
 def search(request):
-    return render(request, 'recipes/pages/search.html')
+    search_term = request.GET.get('q', '').strip()
+    if not search_term:
+        raise Http404()
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}"',
+        'search_term': search_term,
+    })
