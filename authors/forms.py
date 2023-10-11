17c104d43fd4aeef83187f1f2f7fde7ed62f88f8
LuisCaretta <luisfelipecaretta2@gmail.com>
2023-10-11 19:33:33 -0300

    Register form e clean form criados.

--- authors/forms.py
+++ authors/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
 
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
@@ -7,7 +9,18 @@ def add_attr(field, attr_name, attr_new_val):
     
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
-    
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -30,8 +43,10 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter,'
             'one lowercase letter and one number.'
             'The length should be at least 8 characters.'
-        )
+        ),
+        validators = [strong_password],
     )
+    
     password2 = forms.CharField(
         required=True,
         widget=forms.PasswordInput(),
@@ -62,3 +77,17 @@ class RegisterForm(forms.ModelForm):
             }
         }
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+        
+        if password != password2:
+            raise ValidationError({
+                'password': ValidationError('Password and confirm password must be equal.',
+                code = 'invalid'
+                ),
+                'password2': ValidationError('Password and confirm password must be equal.', 
+                code = 'Invalid'
+                )
+            })
